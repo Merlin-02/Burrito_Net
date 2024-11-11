eea61d93d2d6aa393ab5b01076b59b9d44693670
Merlin-02 <kcabrerac2200@alumno.ipn.mx>
2024-11-10 23:04:48 -0600

    listo

--- project_backend/models/User.js
+++ project_backend/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  bio: {String},
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],  // Seguidores
   following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],  // Seguidos
 });
