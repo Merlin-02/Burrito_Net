4b1f7ce695e65d0907e5db16dcdae28ac2912679
josebadillo12 <joseantoniobadillo12@gmail.com>
2024-12-12 18:57:41 -0600

    Cambios visuales en signup y login

--- project_frontend/src/components/Auth/Login.js
+++ project_frontend/src/components/Auth/Login.js
@@ -31,26 +31,79 @@ const Login = () => {
   };
 
   return (
-    <div className="container">
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <input 
-          type="email" 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)} 
-          placeholder="Email"
-          required 
-        />
-        <input 
-          type="password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-          placeholder="Password"
-          required 
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <section 
+      className="position-relative overflow-hidden" 
+      style={{
+        background: 'linear-gradient(135deg, #0798A3 0%, #FCFDF9 100%)',
+        minHeight: '100vh', // Esto asegura que la sección ocupe toda la pantalla
+        padding: 0 // Elimina el padding adicional
+      }}
+    >
+      <div className="container d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+        <div className="row w-100">
+          {/* Imagen y texto en el lado izquierdo */}
+          <div className="col-lg-6 order-last order-lg-first">
+            <div className="mw-md mw-xl-none position-relative">
+              <div className="position-absolute bottom-0 start-0 w-100 p-6">
+                <div className="py-10 px-6 px-sm-10 bg-dark bg-opacity-25 rounded">
+                  <h6 className="text-white text-center">IA para analizar dibujos</h6>
+                  <p className="fs-5 text-white fw-semibold mb-14">
+                    "Sistemas de inteligencia artificial diseñados para interpretar y comprender imágenes... utilizan técnicas de visión por computadora y aprendizaje automático para extraer..."
+                  </p>
+                </div>
+              </div>
+              <img 
+                className="img-fluid d-block w-100" 
+                style={{ minHeight: 620, objectFit: 'cover' }} 
+                src="https://content.cuerpomente.com/medio/2022/09/08/dibujo-nino_6db16aa5_1200x1200.jpg" 
+                alt="Sign Up" 
+              />
+            </div>
+          </div>
+
+          {/* Formulario de Login a la derecha */}
+          <div className="col-lg-6 mb-12 mb-lg-0">
+            <div className="mw-md mw-xl-lg ms-xl-auto">
+              <h5 className="mb-5 text-black text-center">Entra a tu cuenta</h5>
+              <form onSubmit={handleLogin}>
+                <div className="mb-4">
+                  <label className="fw-semibold fs-9 mb-1 text-black" htmlFor="email">Email</label>
+                  <input 
+                    className="form-control" 
+                    type="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} 
+                    id="email" 
+                    placeholder="ejemplo@gmail.com" 
+                    required 
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <label className="fw-semibold fs-9 mb-1 text-black" htmlFor="password">Password</label>
+                  <input 
+                    className="form-control" 
+                    type="password" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} 
+                    id="password" 
+                    placeholder="Introduce tu contraseña" 
+                    required 
+                  />
+                </div>
+
+                <button className="btn btn-primary mb-6 w-100 rounded-pill" type="submit">Login</button>
+
+                <p className="fs-10 fw-semibold text-center text-black">
+                  <span>¿No tienes una cuenta?</span>
+                  <a className="btn btn-link fw-semibold fs-10 p-0" href="/signup">Sign up</a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- project_frontend/src/components/Auth/SignUp.js
+++ project_frontend/src/components/Auth/SignUp.js
@@ -36,33 +36,88 @@ const Signup = () => {
   };
 
   return (
-    <div className="container">
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSignup}>
-        <input 
-          type="text" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} 
-          placeholder="Username" 
-          required 
-        />
-        <input 
-          type="email" 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)} 
-          placeholder="Email" 
-          required 
-        />
-        <input 
-          type="password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-          placeholder="Password" 
-          required 
-        />
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+    <section 
+      className="position-relative overflow-hidden" 
+      style={{
+        background: 'linear-gradient(135deg, #6B133B 0%, #FCFDF9 100%)',
+        minHeight: '100vh', // Esto asegura que la sección ocupe toda la pantalla
+        padding: 0 // Elimina el padding adicional
+      }}
+    >
+      <div className="container d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+        <div className="row w-100">
+          {/* Formulario de Sign Up a la izquierda */}
+          <div className="col-lg-6 mb-12 mb-lg-0">
+            <div className="mw-md mw-xl-lg ms-xl-auto">
+              <h5 className="mb-5 text-black text-center">Crea tu cuenta</h5>
+              <form onSubmit={handleSignup}>
+                <div className="mb-4">
+                  <label className="fw-semibold fs-9 mb-1 text-black" htmlFor="username">Username</label>
+                  <input 
+                    className="form-control" 
+                    type="text" 
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)} 
+                    id="username" 
+                    placeholder="Introduce tu nombre de usuario" 
+                    required 
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <label className="fw-semibold fs-9 mb-1 text-black" htmlFor="email">Email</label>
+                  <input 
+                    className="form-control" 
+                    type="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} 
+                    id="email" 
+                    placeholder="ejemplo@gmail.com" 
+                    required 
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <label className="fw-semibold fs-9 mb-1 text-black" htmlFor="password">Password</label>
+                  <input 
+                    className="form-control" 
+                    type="password" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} 
+                    id="password" 
+                    placeholder="Introduce tu contraseña" 
+                    required 
+                  />
+                </div>
+
+                <button className="btn btn-primary mb-6 w-100 rounded-pill" type="submit">Sign Up</button>
+
+              </form>
+            </div>
+          </div>
+
+          {/* Imagen a la derecha */}
+          <div className="col-lg-6 order-first order-lg-last">
+            <div className="mw-md mw-xl-none position-relative">
+              <div className="position-absolute bottom-0 start-0 w-100 p-6">
+                <div className="py-10 px-6 px-sm-10 bg-dark bg-opacity-25 rounded">
+                  <h6 className="text-white text-center">Nueva Cuenta</h6>
+                  <p className="fs-5 text-white fw-semibold mb-14">
+                    "Crea una cuenta para que puedas subir tus proyecto, conocer más personas, seguirlas y poder compartir mucho conocimiento"
+                  </p>
+                </div>
+              </div>
+              <img 
+                className="img-fluid d-block w-100" 
+                style={{ minHeight: 620, objectFit: 'cover' }} 
+                src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGY0NnZuN2FvZ3IyOXB4eHluOHR5MHhqeGRjZnE4aDc4OGczajI5cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/o0vwzuFwCGAFO/giphy.webp" 
+                alt="Sign Up" 
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
